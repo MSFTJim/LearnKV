235b9baaf80d9c1227589ec28d8dbb83584fe4ca
Jim Vacca <Jim.Vacca@microsoft.com>
2020-09-18 18:55:30 +0000

    broken token

--- Program.cs
+++ Program.cs
@@ -6,6 +6,9 @@ using Microsoft.AspNetCore.Hosting;
 using Microsoft.Extensions.Configuration;
 using Microsoft.Extensions.Hosting;
 using Microsoft.Extensions.Logging;
+using Microsoft.Azure.KeyVault;
+using Microsoft.Azure.Services.AppAuthentication;
+using Microsoft.Extensions.Configuration.AzureKeyVault;
 
 namespace Timmy
 {
@@ -16,11 +19,57 @@ namespace Timmy
             CreateHostBuilder(args).Build().Run();
         }
 
-        public static IHostBuilder CreateHostBuilder(string[] args) =>
+          public static IHostBuilder CreateHostBuilder(string[] args) =>
             Host.CreateDefaultBuilder(args)
-                .ConfigureWebHostDefaults(webBuilder =>
+            .ConfigureAppConfiguration((_, config) =>
+            {
+                var appSettings = config.Build();
+                var KVName = appSettings["KeyVaultName"];
+                var azureServiceTokenProvider = new AzureServiceTokenProvider();
+                
+                var keyVaultClient = new KeyVaultClient(
+                    new KeyVaultClient.AuthenticationCallback(azureServiceTokenProvider.KeyVaultTokenCallback));
+
+                    //https://learnkvjfv.vault.azure.net/
+                    
+                    config.AddAzureKeyVault($"https://{appSettings["KeyVaultName"]}.vault.azure.net/",
+                    keyVaultClient, new DefaultKeyVaultSecretManager());
+            })
+            .ConfigureWebHostDefaults(webBuilder =>
                 {
                     webBuilder.UseStartup<Bobo>();
                 });
+
+//private static string GetKeyVaultEndpoint() => "https://<VAULT_NAME>.vault.azure.net/";
+
+
+    //   public static IHostBuilder CreateHostBuilder(string[] args) =>
+    //         Host.CreateDefaultBuilder(args)
+    //         .ConfigureAppConfiguration((_, config) =>
+    //         {
+    //             var appSettings = config.Build();
+    //             var KVName = appSettings["KeyVaultName"];
+    //             var azureServiceTokenProvider = new AzureServiceTokenProvider();
+    //             var keyVaultClient = new KeyVaultClient(
+    //                 new KeyVaultClient.AuthenticationCallback(azureServiceTokenProvider.KeyVaultTokenCallback));
+    //                 //https://learnkvjfv.vault.azure.net/
+    //                 config.AddAzureKeyVault($"https://{appSettings["KeyVaultName"]}.vault.azure.net/",
+    //                 keyVaultClient, new DefaultKeyVaultSecretManager());
+    //         })
+    //         .ConfigureWebHostDefaults(webBuilder =>
+    //             {
+    //                 webBuilder.UseStartup<Bobo>();
+    //             });
+            
+      
+      
+      
+      
+        // public static IHostBuilder CreateHostBuilder(string[] args) =>
+        //     Host.CreateDefaultBuilder(args)
+        //         .ConfigureWebHostDefaults(webBuilder =>
+        //         {
+        //             webBuilder.UseStartup<Bobo>();
+        //         });
     }
 }
--- Startup.cs
+++ Startup.cs
@@ -23,6 +23,8 @@ namespace Timmy
         // This method gets called by the runtime. Use this method to add services to the container.
         public void ConfigureServices(IServiceCollection services)
         {
+            var KVData = Configuration.GetValue<string>("Message");
+            
             services.AddRazorPages();
         }
 
